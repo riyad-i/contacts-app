5495d27feb6f73f6cd7d83b45ab9741855840733
Riyad <Riyad9876@gmail.com>
2023-08-01 12:56:36 -0400

    contact item

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,12 @@
+
+function ContactItem(props){
+    return(
+        <div>
+            <h3>{props.name}</h3>
+        </div>
+    )
+}
+
+
+
+export default ContactItem;
--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,16 +1,31 @@
 import {useState, useEffect} from 'react'
-
+import ContactItem from "../ContactItem"
 
 function ContactsList(props){
     const [contacts, setContacts] = useState(null)
 
     useEffect( () => {
-        
+        const fetchData = async() => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await res.json()
+            setContacts(data)
+        }
+        fetchData()
     },[])
 
     return(
         <div>
             <h2>Contacts List</h2>
+
+            <>
+                {contacts ? (
+                    contacts.map(contact => <ContactItem name={contact.name} />)
+                ): <h3>No contacts found</h3>
+
+                }
+            
+            </>
+
         </div>
     )
 }
